cf2ccb689838c67a294c88b61e05aa2e8f3dee6c
Bábinszki Botond <babinszkibotond@gmail.com>
2025-01-19 18:27:49 +0100

    Movement 2.0

--- Assets/Scripts/Movement/MouseMovement.cs
+++ Assets/Scripts/Movement/MouseMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float YRotation = 0f;
+
     void Start()
     {
-        
+        //Locking the cursor to the middle of the screen and making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //control rotation around x axis (Look up and down)
+        xRotation -= mouseY;
+
+        //we clamp the rotation so we cant Over-rotate (like in real life)
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //control rotation around y axis (Look up and down)
+        YRotation += mouseX;
+
+        //applying both rotations
+        transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+
     }
 }
