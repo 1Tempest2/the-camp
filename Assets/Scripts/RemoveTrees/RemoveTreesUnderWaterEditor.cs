b467fb3c8257f2547a1d3c5aadc7f9a62b2c894b
Bábinszki Botond <babinszkibotond@gmail.com>
2025-01-20 21:38:46 +0100

    terrain 5.0

---
+++ Assets/Scripts/RemoveTrees/RemoveTreesUnderWaterEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RemoveTreesInGame))]
+public class RemoveUnderwaterTreesEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        RemoveTreesInGame underwaterTreesScript = (RemoveTreesInGame)target;
+
+        if (GUILayout.Button("Remove Underwater Trees"))
+        {
+            underwaterTreesScript.RemoveTreesUnderWater();
+        }
+    }
+}
